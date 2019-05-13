5a0a20eaea2ef5212496b8ef82354b04c86e6605
junkyarddog7 <50583410+junkyarddog7@users.noreply.github.com>
2019-05-13 19:17:50 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apiKey:'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImI4ZDRkMTIwLWMzNWItNGVlZS05MjIxLWI0ZWY0YTUzNDRkNCIsImlhdCI6MTU1Nzc1NDQ5Mywic3ViIjoiZGV2ZWxvcGVyL2MyZDZkODU3LWNmNzMtY2EwZC1lNzc2LWI1MWI0ZTAzOWNjZSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjI3LjYxLjEwNy4yMjIiXSwidHlwZSI6ImNsaWVudCJ9XX0.wtFEyqa-g4fhKz-tcVTMyctaaTJMx0N8jNe8MCeqhbzbv1uD30_xfgS-FW-myTE64yJrK_k6cnUllgOY1UwCBA',
+  discordToken:'NTc3MTI1NjI1OTQxMzI3ODky.XNlyZw.hRtQM5rI-i3k3AOB0s1T9oPPTqM',
+  clans: [
+    {
+      tag:'#L0GRL922',
+      channelID:'577128448569376774',
+      color: 3447003
+    }
+  ],
+  timeDelay: 90,
+  showLeague: false,
+  useRichEmbed: true
+}
